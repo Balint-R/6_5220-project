1b87bd35d50557f299286179910368306f7b90ad
Balint Rozsa <rozsabalint256b@gmail.com>
2025-12-03 17:26:19 -0500

    Remove template

--- src/avx_bf.cpp
+++ src/avx_bf.cpp
@@ -6,34 +6,6 @@ using namespace std;
 #pragma GCC optimize "Ofast"
 
 typedef __m256i m256;
-typedef unsigned uint;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector<pii> vpii;
-typedef complex<double> cpx;
-template <typename T> using minPq = priority_queue<T, vector<T>, greater<T>>;
-#define ms(a, x) memset(a, x, sizeof(a))
-#define pb push_back
-#define fs first
-#define sn second
-#define ALL(v) begin(v), end(v)
-#define SZ(v) ((int) (v).size())
-#define lbv(v, x) (lower_bound(ALL(v), x) - (v).begin())
-#define ubv(v, x) (upper_bound(ALL(v), x) - (v).begin())
-template <typename T> inline void UNIQUE(vector<T> &v){sort(ALL(v)); v.resize(unique(ALL(v)) - v.begin());}
-const int INF = 0x3f3f3f3f;
-const ll LLINF = 0x3f3f3f3f3f3f3f3f;
-const double PI = acos(-1);
-#define FR(i, n) for(int i = 0; i < (n); i++)
-#define FOR(i, a, b) for(int i = (a); i < (b); i++)
-#define FORR(i, a, b) for(int i = (a); i >= (b); i--)
-#define dbg(x) {cerr << #x << ' ' << x << endl;}
-#define dbgArr(arr, n) {cerr << #arr; FR(_i, n) cerr << ' ' << (arr)[_i]; cerr << endl;}
-template <typename T, typename U>
-ostream& operator<<(ostream &os, pair<T, U> p){return os << "(" << p.fs << ", " << p.sn << ")";}
 
 #define vload(ptr) _mm256_loadu_si256((m256*) (ptr))
 #define vstore(ptr, v) _mm256_storeu_si256((m256*) (ptr), (v))
@@ -50,19 +22,19 @@ int main(){
     fread(s2, 1, m+1, stdin);
     s1[n] = s2[m] = 0;
 
-    FR(i, n-m+1){
+    for(int i = 0; i < n-m+1; i++){
         int res = 0;
         int ptr = 0;
 
         constexpr int BSZ = 32*255*4;
-        FR(blk, (m+BSZ-1)/BSZ){
+        for(int blk = 0; blk < (m+BSZ-1)/BSZ; blk++){
             m256 vRes1 = _mm256_setzero_si256();
             m256 vRes2 = _mm256_setzero_si256();
             m256 vRes3 = _mm256_setzero_si256();
             m256 vRes4 = _mm256_setzero_si256();
 
             int numIts = min(255, (m-ptr)/128);
-            FR(j, numIts){
+            for(int j = 0; j < numIts; j++){
                 m256 a1 = vload(s1 + i + ptr + 0);
                 m256 a2 = vload(s1 + i + ptr + 32);
                 m256 a3 = vload(s1 + i + ptr + 64);
@@ -83,12 +55,14 @@ int main(){
             vstore(resArr[1], vRes2);
             vstore(resArr[2], vRes3);
             vstore(resArr[3], vRes4);
-            FR(a, 4) FR(b, 32) res += (uint8_t) (numIts + resArr[a][b]);
+            for(int a = 0; a < 4; a++) for(int b = 0; b < 32; b++){
+                res += (uint8_t) (numIts + resArr[a][b]);
+            }
         }
 
         while(ptr < m) res += s1[i+ptr] != s2[ptr], ptr++;
         ans[i] = res;
     }
 
-    FR(i, n-m+1) printf("%d%c", ans[i], " \n"[i == n-m]);
+    for(int i = 0; i < n-m+1; i++) printf("%d%c", ans[i], " \n"[i == n-m]);
 }
--- src/bf.cpp
+++ src/bf.cpp
@@ -4,35 +4,6 @@ using namespace std;
 #pragma GCC target "avx2"
 #pragma GCC optimize "Ofast"
 
-typedef unsigned uint;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector<pii> vpii;
-typedef complex<double> cpx;
-template <typename T> using minPq = priority_queue<T, vector<T>, greater<T>>;
-#define ms(a, x) memset(a, x, sizeof(a))
-#define pb push_back
-#define fs first
-#define sn second
-#define ALL(v) begin(v), end(v)
-#define SZ(v) ((int) (v).size())
-#define lbv(v, x) (lower_bound(ALL(v), x) - (v).begin())
-#define ubv(v, x) (upper_bound(ALL(v), x) - (v).begin())
-template <typename T> inline void UNIQUE(vector<T> &v){sort(ALL(v)); v.resize(unique(ALL(v)) - v.begin());}
-const int INF = 0x3f3f3f3f;
-const ll LLINF = 0x3f3f3f3f3f3f3f3f;
-const double PI = acos(-1);
-#define FR(i, n) for(int i = 0; i < (n); i++)
-#define FOR(i, a, b) for(int i = (a); i < (b); i++)
-#define FORR(i, a, b) for(int i = (a); i >= (b); i--)
-#define dbg(x) {cerr << #x << ' ' << x << endl;}
-#define dbgArr(arr, n) {cerr << #arr; FR(_i, n) cerr << ' ' << (arr)[_i]; cerr << endl;}
-template <typename T, typename U>
-ostream& operator<<(ostream &os, pair<T, U> p){return os << "(" << p.fs << ", " << p.sn << ")";}
-
 const int MN = 10 << 20;
 int n, m;
 char s1[MN], s2[MN];
@@ -45,11 +16,11 @@ int main(){
     fread(s2, 1, m+1, stdin);
     s1[n] = s2[m] = 0;
 
-    FR(i, n-m+1){
+    for(int i = 0; i < n-m+1; i++){
         int res = 0;
-        FR(j, m) res += s1[i+j] != s2[j];
+        for(int j = 0; j < m; j++) res += s1[i+j] != s2[j];
         ans[i] = res;
     }
 
-    FR(i, n-m+1) printf("%d%c", ans[i], " \n"[i == n-m]);
+    for(int i = 0; i < n-m+1; i++) printf("%d%c", ans[i], " \n"[i == n-m]);
 }
