f4dd3899d4b31f6761afeba1e12d3e21732ff1f7
Anna <anna_y@mit.edu>
2025-12-04 13:07:22 -0500

    added real world data

--- src/hamming_distance_bf.h
+++ src/hamming_distance_bf.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cstdint>
 
-void HammingDistanceBF(size_t n, size_t m, 
+void HammingDistanceBF(size_t n, size_t m,
     const std::vector<uint32_t>& A, const std::vector<uint32_t>& B, std::vector<uint32_t>& dist);
