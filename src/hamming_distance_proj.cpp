971fd52463ffc39fd9da884ddf667782bd3c5e22
Brendon Jiang <brendon.jiang1234567890@gmail.com>
2025-12-05 17:47:58 -0500

    impl'ed everything except fft part

--- src/hamming_distance_proj.cpp
+++ src/hamming_distance_proj.cpp
@@ -1,5 +1,7 @@
 #include "hamming_distance_proj.h"
 #include <cmath>
+#include <algorithm>
+
 
 struct ModPrimeHash {
     uint32_t a;
@@ -22,16 +24,34 @@ void HammingDistanceProj(size_t n, size_t m, size_t sigma, const std::vector<uin
                         std::vector<uint32_t>& dist, std::mt19937& rng) {
     // parameters
     double eps = 0.01;
+    size_t reduced_sigma = std::min(2 / eps, (double) sigma);
     double c = 2; // run c * log n times
+    std::vector<uint32_t> binA(n), binB(m), binOut(n-m+1), cumBinOut(n-m+1); // scratch arrays to run FFT on
 
-    for (size_t round = 0; round < (size_t) (c * std::log2(n)); round ++){ // run for c log n rounds
+    for (size_t round = 0; round < (size_t) (c * std::log2(n)); round++){ // run for c log n rounds
         // pick 2-universal hash function
-        uint32_t p = ModPrimeHash.p;
-        ModPrimeHash h(std::uniform_int_distribution(1, p-1)(rng), std::uniform_int_distribution(0, p-1)(rng));
+        uint32_t p = ModPrimeHash::p;
+        ModPrimeHash h(std::uniform_int_distribution(1u, p-1)(rng), std::uniform_int_distribution(0u, p-1)(rng));
+        cumBinOut.clear();
 
-        // hash alphabet vectors into binary vectors
-        // run fft algorithm
+        // Corollary 4: for each character in reduced alphabet, assign corersponding binary value
+        for (size_t ch = 0; ch < reduced_sigma; ch++){
+            for (size_t i = 0; i < n; i++){
+                binA[i] = ((h(A[i]) % reduced_sigma) == ch);
+            }
+            for (size_t i = 0; i < m; i++){
+                binB[i] = ((h(B[i]) % reduced_sigma) != ch);
+            }
+
+            //TODO: run fft algorithm here and update cumulative
+
+            for (int i = 0; i<n-m+1; i++){
+                cumBinOut[i] += binOut[i];
+            }
+        }
         // update dist array
-
+        for (int i=0; i<n-m+1; i++){
+            dist[i] = std::max(dist[i], cumBinOut[i]);
+        }
     }
 }
