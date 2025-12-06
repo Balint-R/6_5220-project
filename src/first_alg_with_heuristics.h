6637b582bcea73275136b95bb37da78310b85a1f
Anna <anna_y@mit.edu>
2025-12-06 16:02:17 -0500

    removed some functions from first_alg_with_heuristics.h

--- src/first_alg_with_heuristics.h
+++ src/first_alg_with_heuristics.h
@@ -9,10 +9,6 @@
 #include <cassert>
 #include <algorithm>
 
-std::function<int(size_t)> generate_hash(size_t num_buckets);
-
-std::function<int(size_t)> choose_hash_heuristic_1(size_t num_buckets, int k, std::unordered_map<int, long long>& freq);
-
 void HammingDistanceBase(size_t n, size_t m, size_t sigma, const std::vector<uint32_t>& A, const std::vector<uint32_t>& B, std::vector<uint32_t>& dist, std::mt19937& rng);
 
 void HammingDistanceHeuristic_1(size_t n, size_t m, size_t sigma, const std::vector<uint32_t>& A, const std::vector<uint32_t>& B, std::vector<uint32_t>& dist, std::mt19937& rng);
